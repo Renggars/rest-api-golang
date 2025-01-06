a8d7bc24679f02c9f3d38ed9623f0a9f7f4fdd92
Rengga <renggarendy22@gmail.com>
2025-01-06 20:42:30 +0700

    Static Asset

--- routes/index.route.go
+++ routes/index.route.go
@@ -1,15 +1,18 @@
 package routes
 
 import (
+	"github/config"
 	"github/handlers"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func RouteInit(app *fiber.App) {
+	app.Static("/public", config.ProjectRootPath+"/public/assets")
 	app.Post("/user", handlers.UserHandlerCreate)
 	app.Get("/user", handlers.UserHandlerGetAll)
 	app.Get("/user/:id", handlers.UserHandlerGetById)
 	app.Put("/user/:id", handlers.UserHandlerUpdate)
 	app.Put("/user/:id/update-email", handlers.UserHandlerUpdateEmail)
+	app.Delete("/user/:id", handlers.UserHandlerDelete)
 }
