0319642603b14eb3784ee80a1de60fccd6bad6c1
Rengga <renggarendy22@gmail.com>
2025-01-08 19:48:29 +0700

    Middleware

--- routes/index.route.go
+++ routes/index.route.go
@@ -7,12 +7,22 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func middleware(c *fiber.Ctx) error {
+	token := c.Get("x-token")
+	if token != "secret-token" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "unauthorized",
+		})
+	}
+	return c.Next()
+}
+
 func RouteInit(app *fiber.App) {
 	app.Static("/public", config.ProjectRootPath+"/public/assets")
-	app.Post("/user", handlers.UserHandlerCreate)
-	app.Get("/user", handlers.UserHandlerGetAll)
-	app.Get("/user/:id", handlers.UserHandlerGetById)
-	app.Put("/user/:id", handlers.UserHandlerUpdate)
-	app.Put("/user/:id/update-email", handlers.UserHandlerUpdateEmail)
-	app.Delete("/user/:id", handlers.UserHandlerDelete)
+	app.Post("/user", middleware, handlers.UserHandlerCreate)
+	app.Get("/user", middleware, handlers.UserHandlerGetAll)
+	app.Get("/user/:id", middleware, handlers.UserHandlerGetById)
+	app.Put("/user/:id", middleware, handlers.UserHandlerUpdate)
+	app.Put("/user/:id/update-email", middleware, handlers.UserHandlerUpdateEmail)
+	app.Delete("/user/:id", middleware, handlers.UserHandlerDelete)
 }
